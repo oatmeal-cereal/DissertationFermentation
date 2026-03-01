3f97698243dd0387fc6fa962f6e4155e021592bb
Jace <lanapuskas@hotmail.com>
2026-03-01 23:06:15 +0000

    Revert "first"
    This reverts commit 88b588c86a1202cc03602117d2efc1106da7c239.

--- displacy label entities.py
+++
@@ -1,9 +0,0 @@
-import json
-import spacy
-
-with open('list_files\\ner_triples.jsonl', 'r', encoding='utf-8') as f:
-    lines = list(f)
-    
-for line in lines:
-    sent, ents = json.loads(line)
-    print(sent)
--- get scispacy vocab.py
+++
@@ -1,8 +0,0 @@
-import spacy
-import json
-
-model = spacy.load('..\\dissertation DLC content\\en_ner_bionlp13cg_md-0.5.4\\en_ner_bionlp13cg_md\\en_ner_bionlp13cg_md-0.5.4')
-
-print(type(model.vocab.strings))
-
-model.vocab.to_disk('scispacy_vocab')
